52c570b374e7af5606beb0975dc2312dbd7c1baa
Maxime Houis <maxime.houis@epitech.eu>
2021-10-13 23:21:47 +0200

    feat: add option to disable IPO if wanted

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -1,2 +1,3 @@
 option(WARNINGS_AS_ERRORS "Treat compiler warnings as errors" OFF)
 option(${PROJECT_PREFIX}_WITH_SCOPED_PROFILER "Enable scoped profiler" OFF)
+option(${PROJECT_PREFIX}_ENABLE_IPO "Enable InterProcedural Optimizations [Release mode only]" ON)
