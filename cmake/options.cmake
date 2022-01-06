50635bce38473c3c1057212251e665934a404d44
Maxime Houis <maxime.houis@epitech.eu>
2022-01-06 20:01:39 +0100

    other: add project prefix to WarningAsErrors

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -1,3 +1,3 @@
-option(WARNINGS_AS_ERRORS "Treat compiler warnings as errors" OFF)
+option(${PROJECT_PREFIX}_WARNINGS_AS_ERRORS "Treat compiler warnings as errors" OFF)
 option(${PROJECT_PREFIX}_WITH_SCOPED_PROFILER "Enable scoped profiler" OFF)
 option(${PROJECT_PREFIX}_ENABLE_IPO "Enable InterProcedural Optimizations [Release mode only]" ON)
