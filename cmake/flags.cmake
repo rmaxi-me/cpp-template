ea5a2052aca176ec844145761c326eb3c638c712
Maxime Houis <maxime.houis@epitech.eu>
2022-07-23 22:54:23 +0200

    other: update tbb stuff, rename `cli->app`

--- cmake/flags.cmake
+++ cmake/flags.cmake
@@ -1,19 +1,11 @@
-if (CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
-    # template: enable if using std::execution with gcc
+if (CMAKE_CXX_COMPILER_ID STREQUAL "GNU" OR CMAKE_CXX_COMPILER_ID MATCHES ".*Clang")
+    # template: enable if using std::execution with gcc or clang
     #    link_libraries(tbb)
-    #    add_compile_definitions(TBB_SUPPRESS_DEPRECATED_MESSAGES=1) # https://gcc.gnu.org/bugzilla/show_bug.cgi?id=101228
 endif ()
 
 if (MSVC)
     add_link_options(/ignore:4099)
     #    link_libraries(legacy_stdio_definitions)
-endif ()
-
-# TODO: proper std::execution availability check
-if (CMAKE_CXX_COMPILER_ID MATCHES ".*Clang")
-    add_compile_definitions(${PROJECT_PREFIX}_HAS_EXEC_POLICIES=0)
-else ()
-    add_compile_definitions(${PROJECT_PREFIX}_HAS_EXEC_POLICIES=1)
 endif ()
 
 if (${CMAKE_BUILD_TYPE} MATCHES "Debug")
--- format.sh
+++ format.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-find lib cli -name "*.cpp" -or -name "*.hpp" | xargs clang-format -i --verbose
+find lib app -name "*.cpp" -or -name "*.hpp" | xargs clang-format -i --verbose
